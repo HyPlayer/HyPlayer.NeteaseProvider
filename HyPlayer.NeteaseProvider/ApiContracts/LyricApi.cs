40ac7f9a37c545b3b66b8757d34c9aee6a57ef61
kengwang <atkengwang@qq.com>
2023-06-27 00:08:08 +0800

    [feat] Add LyricApi

---
+++ HyPlayer.NeteaseProvider/ApiContracts/LyricApi.cs
@@ -0,0 +1,24 @@
+﻿using HyPlayer.NeteaseProvider.ActualRequests;
+using HyPlayer.NeteaseProvider.Bases;
+using HyPlayer.NeteaseProvider.Bases.ApiContractBases;
+using HyPlayer.NeteaseProvider.Requests;
+using HyPlayer.NeteaseProvider.Responses;
+
+namespace HyPlayer.NeteaseProvider.ApiContracts;
+
+public class LyricApi : EApiContractBase<LyricRequest, LyricResponse, ErrorResultBase, LyricActualRequest>
+{
+    public override string Url => "https://interface3.music.163.com/eapi/song/lyric/v1";
+    public override HttpMethod Method => HttpMethod.Post;
+
+    public override Task MapRequest(LyricRequest request)
+    {
+        ActualRequest = new()
+                        {
+                            Id = request.Id
+                        };
+        return Task.CompletedTask;
+    }
+
+    public override string ApiPath => "/api/song/lyric/v1";
+}
---
+++ HyPlayer.NeteaseProvider/Responses/LyricResponse.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+using HyPlayer.NeteaseProvider.Bases;
+
+namespace HyPlayer.NeteaseProvider.Responses;
+
+public class LyricResponse : CodedResponseBase
+{
+
+    [JsonPropertyName("transUser")] public LyricUserInfo? TranslationUser { get; set; }
+    [JsonPropertyName("lyricUser")] public LyricUserInfo? LyricUser { get; set; }
+    [JsonPropertyName("lrc")] public LyricInfo? Lyric { get; set; }
+    [JsonPropertyName("klyric")] public LyricInfo? OldKaraokLyric { get; set; }
+    [JsonPropertyName("tlyric")] public LyricInfo? TranslationLyric { get; set; }
+    [JsonPropertyName("romalrc")] public LyricInfo? RomajiLyric { get; set; }
+
+    [JsonPropertyName("yrc")] public LyricInfo? YunLyric { get; set; }
+    [JsonPropertyName("ytlrc")] public LyricInfo? YunTranslationLyric { get; set; }
+    [JsonPropertyName("yromalrc")] public LyricInfo? YunRomajiLyric { get; set; }
+    
+    
+
+    public class LyricInfo
+    {
+        [JsonPropertyName("version")] public int Version { get; set; }
+        [JsonPropertyName("lyric")] public string? Lyric { get; set; }
+    }
+
+    public class LyricUserInfo
+    {
+        [JsonPropertyName("id")] public string? LyricId { get; set; }
+        [JsonPropertyName("userid")] public string? UserId { get; set; }
+        [JsonPropertyName("nickname")] public string? Nickname { get; set; }
+        [JsonPropertyName("uptime")] public long UpdateTime { get; set; }
+    }
+}
