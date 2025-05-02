77f60d7493fc8907c35e295c52ddbf749978fba2
Kengwang <github@kengwang.com.cn>
2025-05-03 00:49:44 +0800

    feat: 新增一起听 API

---
+++ HyPlayer.NeteaseApi/ApiContracts/ListenTogether/Dual/ListenTogetherInvitationAcceptApi.cs
@@ -0,0 +1,55 @@
+using System.Text.Json.Serialization;
+using HyPlayer.NeteaseApi.ApiContracts.Category;
+using HyPlayer.NeteaseApi.ApiContracts.ListenTogether.Dual;
+using HyPlayer.NeteaseApi.Bases;
+using HyPlayer.NeteaseApi.Bases.ApiContractBases;
+
+namespace HyPlayer.NeteaseApi.ApiContracts
+{
+    public static partial class NeteaseApis
+    {
+        public static ListenTogetherInvitationAcceptApi ListenTogetherInvitationAcceptApi => new();
+    }
+}
+
+
+namespace HyPlayer.NeteaseApi.ApiContracts.Category
+{
+    public class ListenTogetherInvitationAcceptApi : EApiContractBase<ListenTogetherInvitationAcceptRequest,
+        ListenTogetherInvitationAcceptResponse, ErrorResultBase, ListenTogetherInvitationAcceptActualRequest>
+    {
+        public override string IdentifyRoute => "/listentogether/invitation/accept";
+
+        public override string Url { get; protected set; } =
+            "https://interface3.music.163.com/eapi/listen/together/play/invitation/accept";
+
+        public override HttpMethod Method => HttpMethod.Post;
+
+        public override Task MapRequest()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override string ApiPath { get; protected set; } = "/api/listen/together/play/invitation/accept";
+    }
+
+    public class ListenTogetherInvitationAcceptRequest : RequestBase
+    {
+        public required string RoomId { get; set; }
+        public required string InviterId { get; set; }
+        public string Refer { get; set; } = "third_party_invite";
+    }
+
+    public class ListenTogetherInvitationAcceptResponse : CodedResponseBase
+    {
+        [JsonPropertyName("data")]
+        public ListenTogetherRoomCreateResponse.ListenTogetherRoomCreateResponseData? Data { get; set; }
+    }
+
+    public class ListenTogetherInvitationAcceptActualRequest : EApiActualRequestBase
+    {
+        [JsonPropertyName("roomId")] public required string RoomId { get; set; }
+        [JsonPropertyName("inviterId")] public required string InviterId { get; set; }
+        [JsonPropertyName("refer")] public string Refer { get; set; } = "third_party_invite";
+    }
+}
---
+++ HyPlayer.NeteaseApi/ApiContracts/ListenTogether/Dual/ListenTogetherStatusApi.cs
@@ -0,0 +1,55 @@
+using System.Text.Json.Serialization;
+using HyPlayer.NeteaseApi.ApiContracts.Category;
+using HyPlayer.NeteaseApi.ApiContracts.ListenTogether.Dual;
+using HyPlayer.NeteaseApi.Bases;
+using HyPlayer.NeteaseApi.Bases.ApiContractBases;
+
+namespace HyPlayer.NeteaseApi.ApiContracts
+{
+
+public static partial class NeteaseApis
+{
+    public static ListenTogetherStatusApi ListenTogetherStatusApi => new();
+}
+}
+
+
+namespace HyPlayer.NeteaseApi.ApiContracts.Category
+{
+
+    public class ListenTogetherStatusApi : EApiContractBase<ListenTogetherStatusRequest, ListenTogetherStatusResponse, ErrorResultBase, ListenTogetherStatusActualRequest>
+    {
+        public override string IdentifyRoute => "/listentogether/status/get";
+        public override string Url { get; protected set; } = "https://interface3.music.163.com/eapi/listen/together/status/get";
+        public override HttpMethod Method => HttpMethod.Post;
+
+        public override Task MapRequest()
+        {
+            return Task.CompletedTask;
+        }
+
+        public override string ApiPath { get; protected set; } = "/api/listen/together/status/get";
+    }
+
+    public class ListenTogetherStatusRequest : RequestBase
+    {
+
+    }
+
+    public class ListenTogetherStatusResponse : CodedResponseBase
+    {
+        [JsonPropertyName("data")] public ListenTogetherStatusResponseData? Data { get; set; }
+
+        public class ListenTogetherStatusResponseData
+        {
+            [JsonPropertyName("inRoom")] public bool IsInRoom { get; set; }
+            [JsonPropertyName("roomInfo")] public ListenTogetherRoomCreateResponse.ListenTogetherRoomInfo? RoomInfo { get; set; }
+            [JsonPropertyName("status")] public string? Status { get; set; }
+        }
+    }
+
+    public class ListenTogetherStatusActualRequest : EApiActualRequestBase
+    {
+
+    }
+}
