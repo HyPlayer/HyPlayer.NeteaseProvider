2dd32374191c28e248132772196225478b71e2e2
zhh134 <zhh13950510580@outlook.com>
2026-02-05 21:26:07 +0800

    Feat<Api>: 添加VideoSubscribeApi、VideoUnsubscribeApi.

---
+++ HyPlayer.NeteaseApi/ApiContracts/Video/VideoSubscribeApi.cs
@@ -0,0 +1,53 @@
+﻿using HyPlayer.NeteaseApi.ApiContracts.Video;
+using HyPlayer.NeteaseApi.Bases;
+using HyPlayer.NeteaseApi.Bases.EApiContractBases;
+using System.Text.Json.Serialization;
+
+namespace HyPlayer.NeteaseApi.ApiContracts
+{
+    public static partial class NeteaseApis
+    {
+        public static VideoSubscribeApi VideoSubscribeApi => new();
+    }
+}
+
+namespace HyPlayer.NeteaseApi.ApiContracts.Video
+{
+
+    public class VideoSubscribeApi : EApiContractBase<VideoSubscribeRequest, VideoSubscribeResponse, ErrorResultBase,
+        VideoSubscribeActualRequest>
+    {
+        public override string IdentifyRoute => "/mv/subscribe";
+        public override string Url { get; protected set; } = "https://interface.music.163.com/eapi/mv/sub";
+        public override HttpMethod Method => HttpMethod.Post;
+
+        public override Task MapRequest(ApiHandlerOption option)
+        {
+            if (Request is not null)
+            {
+                ActualRequest = new VideoSubscribeActualRequest
+                {
+                    MvId = Request.MvId
+                };
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public override string ApiPath { get; protected set; } = "/api/mv/sub";
+    }
+
+    public class VideoSubscribeRequest : RequestBase
+    {
+        public required string MvId { get; set; }
+    }
+
+    public class VideoSubscribeResponse : CodedResponseBase
+    {
+    }
+
+    public class VideoSubscribeActualRequest : EApiActualRequestBase
+    {
+        [JsonPropertyName("mvId")] public required string MvId { get; set; }
+    }
+}
---
+++ HyPlayer.NeteaseApi/ApiContracts/Video/VideoUnsubscribeApi.cs
@@ -0,0 +1,59 @@
+﻿using HyPlayer.NeteaseApi.Bases;
+using HyPlayer.NeteaseApi.Bases.EApiContractBases;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using HyPlayer.NeteaseApi.ApiContracts.Video;
+
+namespace HyPlayer.NeteaseApi.ApiContracts
+{
+    public static partial class NeteaseApis
+    {
+        public static VideoUnsubscribeApi VideoUnsubscribeApi => new();
+    }
+}
+
+namespace HyPlayer.NeteaseApi.ApiContracts.Video
+{
+    public class VideoUnsubscribeApi : EApiContractBase<VideoUnsubscribeRequest, VideoUnsubscribeResponse, ErrorResultBase,
+        VideoUnsubscribeActualRequest>
+    {
+        public override string ApiPath { get; protected set; } = "/api/video/unsub";
+
+        public override string IdentifyRoute => "/video/unsub";
+
+        public override string Url { get; protected set; } = "https://interface.music.163.com/eapi/video/";
+
+        public override HttpMethod Method => HttpMethod.Post;
+
+        public override Task MapRequest(ApiHandlerOption option)
+        {
+            if (Request is not null)
+            {
+                ActualRequest = new VideoUnsubscribeActualRequest
+                {
+                    // serialize C# array of ids to JSON array string expected by the API
+                    VideoIds = JsonSerializer.Serialize(Request.VideoIds)
+                };
+                Url += "unsub";
+            }
+            return Task.CompletedTask;
+        }
+    }
+
+    public class VideoUnsubscribeRequest : RequestBase
+    {
+        /// <summary>
+        /// Array of artist ids. Will be converted to a JSON array string in the request, e.g. new long[] { 35374786 } -> "[35374786]"
+        /// </summary>
+        public required string[] VideoIds { get; set; }
+    }
+
+    public class VideoUnsubscribeResponse : CodedResponseBase
+    {
+    }
+
+    public class VideoUnsubscribeActualRequest : EApiActualRequestBase
+    {
+        [JsonPropertyName("mvIds")] public required string VideoIds { get; set; }
+    }
+}
